407e8b6fcbc489fb3eca2a74a67f670090f7d9e5
pawcza <pczarniecki@genesis.com.mt>
2020-12-17 15:00:56 +0100

    styles investigation

--- brand1/components/Button/index.js
+++ brand1/components/Button/index.js
@@ -3,14 +3,17 @@ import styled, { css } from 'styled-components'
 import OriginalButton from 'common/components/Button';
 
 const Button = styled(OriginalButton)`
-  border-color: green;
-  background-color: lightgreen;
-  
-  // Kind
-  ${props => props.secondary && css`
-    border-color: blue;
-    background-color: lightblue;
-  `}
+  && {
+    border-color: green;
+    background-color: lightgreen;
+    
+    // Kind
+    ${props => props.secondary && css`
+      border-color: blue;
+      background-color: lightblue;
+    `}
+  }
+ 
 `
 
 export default Button;
