31dd2efeee2c85387c76f7d80934f2c498007e47
pawcza <pczarniecki@genesis.com.mt>
2020-12-22 14:59:29 +0100

    language switcher

--- common/components/Menu/index.js
+++ common/components/Menu/index.js
@@ -1,35 +1,40 @@
 import Link from 'next/link';
 
 const Menu = () => {
-  return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/fetchingDataStatic">
-          <a>Static Fetching data excample</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/fetchingDataServer">
-          <a>Server Fetching data excample</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/promotions">
-          <a>Promotions</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/promotions/aaa">
-          <a>Promotion Aaa</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    return (
+        <ul>
+            <li>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/fetchingDataStatic">
+                    <a>Static Fetching data excample</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/fetchingDataServer">
+                    <a>Server Fetching data excample</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/promotions">
+                    <a>Promotions</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/promotions/aaa">
+                    <a>Promotion Aaa</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/language">
+                    <a>Languages</a>
+                </Link>
+            </li>
+        </ul>
+    )
 };
 
 export default Menu;
